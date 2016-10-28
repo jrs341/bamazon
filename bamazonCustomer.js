cf619b53ff92fc894bdbbc1f25fdfd1b247faebb
Jason Sinn <jrs341@me.com>
2016-10-28 13:57:12 -0500

    added function promptCustomer to receiv user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var prompt = require('prompt');
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,10 +11,40 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    // console.log("connected as id " + connection.threadId);
 })
 
-connection.query('SELECT * FROM products', function(err, res) {
-    if (err) throw err;
-    console.log(res);
-})
+var promptCustomer = function() {
+	prompt.start();
+  // Get two properties from the user: productID and quantity 
+  prompt.get(['productID', 'quantity'], function (err, result) { 
+    // Log the results. 
+    console.log('Command-line input received:');
+    console.log('  product ID: ' + result.productID);
+    console.log('  quantity: ' + result.quantity);
+  });
+}
+var displayProducts = function() {
+	connection.query('SELECT * FROM products', function(err, res) {
+	    if (err) throw err;
+	    console.log(res); 
+	    promptCustomer();   
+	});
+}
+
+displayProducts();
+
+// prompt.start();
+ 
+//   // 
+//   // Get two properties from the user: username and email 
+//   // 
+//   prompt.get(['productID', 'quantity'], function (err, result) {
+//     // 
+//     // Log the results. 
+//     // 
+//     console.log('Command-line input received:');
+//     console.log('  product ID: ' + result.productID);
+//     console.log('  quantity: ' + result.quantity);
+//   });
+
