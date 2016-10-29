29f4373f50b010057b7694c564e08884193d4f5d
Jason Sinn <jrs341@me.com>
2016-10-28 22:03:34 -0500

    added a search function with a inquirer list and switch case

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,4 +12,39 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    runSearch();
 })
+
+var runSearch = function() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View Products for Sale", 
+            "View Low Inventory", 
+            "Add to Inventory", 
+            "Add New Product" 
+        ]
+    }).then(function(answer) {
+    	// console.log(answer.action);
+        switch(answer.action) {
+            case 'View Products for Sale':
+            	console.log('products for sale');
+                // artistSearch();
+            break;
+
+            case 'View Low Inventory':
+                // multiSearch();
+            break;
+
+            case 'Add to Inventory':
+                // rangeSearch();
+            break;
+
+            case 'Add New Product':
+                // songSearch();
+            break;
+        }
+    })
+}
